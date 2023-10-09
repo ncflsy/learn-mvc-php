20580475cb5b50f6b518911dcd5788577b08b989
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-10-10 06:59:56 +0700

    add

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,17 @@ class App
 {
     public function __construct()
     {
-        echo 'halooo';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
